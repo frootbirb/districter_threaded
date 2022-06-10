6fe104f08a07501059e671a3a40b8e92235626f4
frootbirb <abbycat1000@gmail.com>
2022-06-10 00:38:11 -0400

    Initial work! It runs, with a lot of shortcuts

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace districter_threaded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Globals._read();
+            Program.solve("states", "Area (mi2)");
+        }
+
+        static public void solve(string scale, string metricID) 
+        {
+            Globals.scale = scale;
+            Globals.metricID = metricID;
+            State state = new State(2);
+            while (!state.isDone()) 
+            {
+                state.doStep();
+            }
+
+            state.groups.ForEach(g => g.print());
+            Console.WriteLine(state.groups.Sum(g => g.units.Count));
+        }
+    }
+}
